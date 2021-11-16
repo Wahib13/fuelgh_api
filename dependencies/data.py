b4d1b8eb66a366cc93d2d9e1e7782eaeefc93d70
Wahib Farhat <wahibfarhat13@gmail.com>
2021-11-16 10:28:32 +0000

    updated compose file with correct ports to suite dev environment

--- dependencies/data.py
+++ dependencies/data.py
@@ -45,7 +45,7 @@ async def query_delete_omc(_id: str):
 
 fake_users_db = {
     "johndoe": {
-        "username": "johndoe",
+        "username": "johndoe1",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
